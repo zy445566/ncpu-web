1d5f7b0ea515b7c6d05911a26dc3cf57f49e240b
zy445566 <zyct@vip.qq.com>
2020-09-07 16:42:41 +0800

    add ci

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  mode:'production',
   entry: path.join(__dirname, 'build','test.js'),
   output: {
     path: path.resolve(__dirname, 'static'),
