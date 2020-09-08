962d62aed3edac32c8fc014f53dc68b47f03a85b
zy445566 <zyct@vip.qq.com>
2020-09-08 11:17:01 +0800

    optimize build 2 ts

---
+++ build.ts
@@ -0,0 +1,9 @@
+const { readFileSync, writeFileSync, unlinkSync } = require('fs');
+const { join } = require('path');
+const workerPath = join(__dirname, 'worker', 'web-worker.js');
+const workerIndexPath = join(__dirname, 'worker', 'index.js');
+const workerData = readFileSync(workerPath).toString();
+const workerIndexData = readFileSync(workerIndexPath).toString();
+writeFileSync(workerIndexPath,workerIndexData.replace(/['"`]{1}REPLACE_JS_DATA['"`]{1}/,JSON.stringify(workerData)));
+unlinkSync(workerPath);
+unlinkSync(__filename);
