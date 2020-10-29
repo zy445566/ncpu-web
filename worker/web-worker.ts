568ca18c5fc8f0dade939b2881d38dd5c150ebe2
zy445566 <zyct@vip.qq.com>
2020-10-29 11:18:06 +0800

    feature: stronger error tracing

--- worker/web-worker.ts
+++ worker/web-worker.ts
@@ -7,12 +7,19 @@ type NcpuResult = {
     key:number,
     res:any
 }
+var runFunction = function(_params){}
 self.onmessage =  async function(event) {
     const ncpuParams:NcpuParams = event.data;
-    const runFunction = new Function(
-        'params',
-        `const func = ${ncpuParams.functionData};return func(...params);`
+    const blob = new Blob(
+        [`function runFunction (params){const func = ${ncpuParams.functionData};return func(...params)}`], 
+        {type : 'application/javascript'}
     );
+    const workerUrl = URL.createObjectURL(blob);
+    importScripts(workerUrl)
+    // const runFunction = new Function(
+    //     'params',
+    //     `const func = ${ncpuParams.functionData};return func(...params);`
+    // );
     const result:NcpuResult = {key:ncpuParams.key,res:undefined}
     result.res = runFunction(ncpuParams.params);
     if(result.res instanceof Promise) {result.res = await result.res;}
