c97203b098f7ca7c9139553a846d629be2e8e404
zy445566 <zyct@vip.qq.com>
2020-10-29 11:40:09 +0800

    fix: more stable

--- worker/web-worker.ts
+++ worker/web-worker.ts
@@ -7,19 +7,21 @@ type NcpuResult = {
     key:number,
     res:any
 }
-var runFunction = function(_params){}
+// var runFunction = function(_params){}
 self.onmessage =  async function(event) {
     const ncpuParams:NcpuParams = event.data;
-    const blob = new Blob(
-        [`function runFunction (params){const func = ${ncpuParams.functionData};return func(...params)}`], 
-        {type : 'application/javascript'}
-    );
-    const workerUrl = URL.createObjectURL(blob);
-    importScripts(workerUrl)
-    // const runFunction = new Function(
-    //     'params',
-    //     `const func = ${ncpuParams.functionData};return func(...params);`
+    // more error tracing
+    // const blob = new Blob(
+    //     [`function runFunction (params){const func = ${ncpuParams.functionData};return func(...params)}`], 
+    //     {type : 'application/javascript'}
     // );
+    // const workerUrl = URL.createObjectURL(blob);
+    // importScripts(workerUrl)
+    // more stable
+    const runFunction = new Function(
+        'params',
+        `const func = ${ncpuParams.functionData};return func(...params);`
+    );
     const result:NcpuResult = {key:ncpuParams.key,res:undefined}
     result.res = runFunction(ncpuParams.params);
     if(result.res instanceof Promise) {result.res = await result.res;}
